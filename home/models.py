148997a82c5470854dbe93e8dbfd120080c2260e
Sean Harrison <sharrison@caktusgroup.com>
2019-05-03 09:16:29 -0400

    ExamplePage adding pages with docs in the 'docs' field

--- home/models.py
+++ home/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.db.models import ImageField
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core.blocks import RichTextBlock, PageChooserBlock
 from wagtail.core.models import Page
 from wagtail.documents.blocks import DocumentChooserBlock
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 
@@ -22,6 +24,7 @@ class ExamplePage(Page):
     body = RichTextField()
     docs = StreamField([
         ('doc', DocumentChooserBlock()),
+        ('page', PageChooserBlock()),
     ])
     content_panels = Page.content_panels + [
         ImageChooserPanel('image'),
