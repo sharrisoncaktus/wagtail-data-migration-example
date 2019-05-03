9e87301fa060ceca05a7f43e0ca389dab25b374e
Sean Harrison <sharrison@caktusgroup.com>
2019-05-03 09:17:13 -0400

    ExamplePage – add content streamfield

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models import ImageField
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.fields import RichTextField, StreamField
-from wagtail.core.blocks import RichTextBlock, PageChooserBlock
+from wagtail.core.blocks import RichTextBlock, PageChooserBlock, StreamBlock
 from wagtail.core.models import Page
 from wagtail.documents.blocks import DocumentChooserBlock
 from wagtail.images.blocks import ImageChooserBlock
@@ -14,19 +14,36 @@ class HomePage(Page):
 
 
 class ExamplePage(Page):
+    content = StreamField([
+        ('image', ImageChooserBlock()),
+        ('text', RichTextBlock()),
+        ('docs',
+         StreamBlock([
+             ('doc', DocumentChooserBlock()),
+             ('image', PageChooserBlock()),
+         ])),
+    ],
+                          blank=True,
+                          null=True)
+
+    # old fields retained for now
     image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name='+'
+        related_name='+',
     )
     body = RichTextField()
     docs = StreamField([
         ('doc', DocumentChooserBlock()),
         ('page', PageChooserBlock()),
     ])
+
     content_panels = Page.content_panels + [
+        StreamFieldPanel('content'),
+
+        # old panels retained for now
         ImageChooserPanel('image'),
         FieldPanel('body'),
         StreamFieldPanel('docs'),
